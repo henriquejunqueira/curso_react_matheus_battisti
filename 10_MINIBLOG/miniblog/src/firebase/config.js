1968e94ed32885eeda7a6914df9d5b96f558949d
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-05-26 17:52:08 -0300

    authentication

---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYvCl3jhSofgx2rlBs09ey_91b72__f18',
+  authDomain: 'miniblog-cc382.firebaseapp.com',
+  projectId: 'miniblog-cc382',
+  storageBucket: '193119714124',
+  appId: '1:193119714124:web:0932ebcbd52fe00ed86de9',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
---
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.js
@@ -0,0 +1,103 @@
+import { useAuthentication } from '../../hooks/useAuthentication';
+import styles from './Register.module.css';
+
+import { useState, useEffect } from 'react';
+
+const Register = () => {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassoword] = useState('');
+  const [error, setError] = useState('');
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setError('');
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError('As senhas precisam ser iguais!');
+      return;
+    }
+
+    const response = await createUser(user);
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartihe suas histórias</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome de usuário..."
+            value={displayName}
+            onChange={(event) => setDisplayName(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Email de usuário..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha..."
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha..."
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassoword(event.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Register;
